b6235d4b600c1967781f05701a3a9c4f1ac26785
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2022-10-13 17:59:26 -0700

    100% test coverage. Yay!

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ export { default as SpielNode } from 'types/SpielNode';
 export { default as SpielLine } from 'types/SpielLine';
 export { default as SpielReply } from 'types/SpielReply';
 export { default as SpielManager } from 'traversal/SpielManager';
+
+/* This file only imports and reexports top-level APIs and has been excluded from Jest 
+   coverage reporting in package.json. All the exports are tested via unit tests associated
+   with the import module. */
