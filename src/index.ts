47563f8d31a7aa7fc61c9755d816a6ee147cc5f7
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2023-02-06 18:17:39 -0800

    Added some exports.

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ export * from './impex/glossaryHtmlUtil';
 export * from './analysis/findUtil';
 
 export { default as Spiel, repairSpiel } from './types/Spiel';
-export { default as SpielNode } from './types/SpielNode';
+export { default as SpielNode, duplicateSpielNode } from './types/SpielNode';
 export { default as SpielLine } from './types/SpielLine';
 export { default as SpielReply } from './types/SpielReply';
+export { default as Emotion } from './types/Emotion';
 
 /* This file only imports and re-exports top-level APIs and has been excluded from Jest 
    coverage reporting in package.json. All the exports are tested via unit tests associated
